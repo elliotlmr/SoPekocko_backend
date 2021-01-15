383fa29c41eaae119691bcd590fa84eee87d4d1b
Elliot Lemaire <lemaireelliot@wanadoo.fr>
2021-01-15 12:15:02 +0100

    Correction erreur header

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -55,174 +55,26 @@ exports.modifyLike = (req, res, next) => {
     const liked = req.body.like;
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            const likes = sauce.likes;
-            const dislikes = sauce.dislikes;
-            const usersLiked = sauce.usersLiked;
-            const usersDisliked = sauce.usersDisliked;
             if (liked == 1 || liked == -1) {
-                sauce[liked == 1 ? likes : dislikes] += 1;
-                sauce[liked == 1 ? usersLiked.push(req.body.userId) : usersDisliked.push(req.body.userId)];
-                /*sauce[liked == 1 && sauce.usersLiked.includes(req.body.userId) ? ]
-                if (sauce.usersLiked.includes(req.body.userId)) {
-                    sauce.usersLiked.filter(user => user != req.body.userId);
-                    sauce.likes -= 1;
-                }
-                if (sauce.usersDisliked.includes(req.body.userId)) {
-                    sauce.usersDisliked.filter(user => user != req.body.userId);
-                    sauce.dislikes -= 1;
-                }*/
+                liked == 1 ? (
+                    sauce.likes += 1, 
+                    sauce.usersLiked.push(req.body.userId)
+                ) : (
+                    sauce.dislikes += 1, 
+                    sauce.usersDisliked.push(req.body.userId)
+                );
             } else {
-                if (sauce.usersLiked.includes(req.body.userId)) {
-                    sauce.likes -= 1;
-                    sauce.usersLiked.filter(user => user != req.body.userId);
-                };
-                if (sauce.usersDisliked.includes(req.body.userId)) {
-                    sauce.dislikes -= 1;
-                    sauce.usersDisliked.filter(user => user != req.body.userId);
-                };
-                /*sauce[sauce.usersLiked.includes(req.body.userId) ? likes : dislikes] -= 1;
-                sauce[sauce.usersLiked.includes(req.body.userId) ? usersLiked : usersDisliked].filter(user => user != req.body.userId);
-                */
+                sauce.usersLiked.includes(req.body.userId) ? (
+                    sauce.likes -= 1,
+                    sauce.usersLiked.filter(user => user !== req.body.userId)
+                ) : (
+                    sauce.dislikes -= 1,
+                    sauce.usersDisliked.filter(user => user !== req.body.userId)
+                );
             };
             Sauce.updateOne({ _id: req.params.id }, sauce)
                 .then(() => res.status(200).json({ message: 'Objet modifié like/dislike !' }))
                 .catch(error => console.log(error));
-                res.status(400).json({ message: 'Impossible de modifier les likes' });
         })
         .catch(error => console.log(error));
-        res.status(400).json({ message: 'Impossible de like/dislike' });
 };
-
-/*exports.modifyLike = (req, res, next) => {
-    const liked = req.body.like;
-    if (liked == 1 || liked == -1) {
-        Sauce.findOne({ _id: req.params.id })
-            .then(sauce => {
-                sauce[liked == 1 ? likes : dislikes] += 1;
-                sauce[liked == 1 ? usersLiked : userDisliked].push(req.body.userId);
-                if (sauce.usersLiked.includes(req.body.userId)) {
-                    sauce.userLiked.filter(user => user != req.body.userId);
-                    sauce.likes -= 1;
-                }
-                if (sauce.usersDisiked.includes(req.body.userId)) {
-                    sauce.userDisliked.filter(user => user != req.body.userId);
-                    sauce.dislikes -= 1;
-                }
-                Sauce.updateOne({ _id: req.params.id }, sauce)
-                    .then(() => res.status(200).json({ message: 'Objet modifié like/dislike !' }))
-                    .catch(error => res.status(400).json({ message: 'Impossible de modifier les likes' }));
-            })
-            .catch(error => res.status(400).json({ error }));
-    } else {
-        Sauce.findOne({ _id: req.params.id })
-            .then(sauce => {
-                sauce[usersLiked.includes(req.body.userId) ? likes : dislikes] -= 1;
-                sauce[usersLiked.includes(req.body.userId) ? usersLiked : usersDisiked].filter(user => user != req.body.userId);
-                Sauce.updateOne({ _id: req.params.id }, sauce)
-                    .then(() => res.status(200).json({ message: 'Objet modifié like/dislike !' }))
-                    .catch(error => res.status(400).json({ message: 'Impossible de modifier les likes' }));
-            })
-            .catch(error => res.status(400).json({ error }));
-    };
-};
-
-
-/*exports.modifyLike = (req, res, next) => {
-    const liked = req.body.like;
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-            if (liked == 1 || liked == -1) {
-                sauce[liked == 1 ? likes : dislikes] += 1;
-                sauce[liked == 1 ? usersLiked : userDisliked].push(req.body.userId);
-                if (sauce.usersLiked.includes(req.body.userId)) {
-                    sauce.userLiked.filter(user => user != req.body.userId);
-                    sauce.likes -= 1;
-                }
-                if (sauce.usersDisiked.includes(req.body.userId)) {
-                    sauce.userDisliked.filter(user => user != req.body.userId);
-                    sauce.dislikes -= 1;
-                }
-            } else {
-                sauce[usersLiked.includes(req.body.userId) ? likes : dislikes] -= 1;
-                sauce[usersLiked.includes(req.body.userId) ? usersLiked : usersDisiked].filter(user => user != req.body.userId);
-            };
-            Sauce.updateOne({ _id: req.params.id }, sauce)
-                .then(() => res.status(200).json({ message: 'Objet modifié like/dislike !' }))
-                .catch(error => res.status(400).json({ message: 'Impossible de modifier les likes' }));
-            console.log(sauce, error)
-        })
-        .catch(error => res.status(400).json({ message: 'Impossible de like/dislike' }));
-};
-
-
-exports.modifyLike = (req, res, next) => {
-    const sauceObject = req.file ?
-        {
-            ...JSON.parse(req.body.sauce),
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-        } : { ...req.body };
-    Sauce.updateOne({ _id: req.params.id },
-        (err, doc) => {
-            if (err) console.log(err)
-            const alreadyLiked = sauceObject.usersLiked.some(x => x._id == req.user.id);
-            const alreadyDisliked = sauceObject.usersDisliked.some(x => x._id == req.user.id);
-
-            if (alreadyLiked) {
-                User.updateOne({
-                    'sauces._id': req.params.id
-                },
-                    {
-                        $inc: { 'sauces.$.likes': 0 },
-                        $pull: { 'sauces.$.usersLiked': { _id: req.user.id } }
-                    },
-                    err => {
-                        if (err) console.log(err)
-                        res.send('nomoreliked');
-                    });
-            } else {
-                User.updateOne({
-                    'sauces._id': req.params.id
-                },
-                    {
-                        $inc: { 'sauces.$.likes': 1 },
-                        $push: { 'posts.$.usersLiked': { _id: req.user.id } }
-                    },
-                    err => {
-                        if (err) console.log(err)
-                        res.send('liked');
-                    });
-            };
-            alreadyLiked.save()
-                .then(() => res.status(201).json({ message: 'Objet modifié (like/dislike) !' }))
-                .catch(error => res.status(400).json({ error }));
-
-            if (alreadyDisliked) {
-                User.updateOne({
-                    'sauces._id': req.params.id
-                    },
-                    {
-                        $inc: { 'sauces.$.dislikes': 0 },
-                        $pull: { 'sauces.$.usersDisliked': { _id: req.user.id } }
-                    },
-                    err => {
-                        if (err) console.log(err)
-                        res.send('nomoredisliked');
-                    });
-            } else {
-                User.updateOne({
-                    'sauces._id': req.params.id
-                    },
-                    {
-                        $inc: { 'sauces.$.dislikes': 1 },
-                        $push: { 'posts.$.usersDisliked': { _id: req.user.id } }
-                    },
-                    err => {
-                        if (err) console.log(err)
-                        res.send('disliked');
-                    });
-            };
-            alreadyDisliked.save()
-                .then(() => res.status(201).json({ message: 'Objet modifié (like/dislike) !' }))
-                .catch(error => res.status(400).json({ error }));
-        });
-};*/
